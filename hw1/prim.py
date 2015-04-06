f7e512cf56f662b1e376d3d56577b0d1ea1f3be7
Benjamin Hipple <brh2113@columbia.edu>
2015-04-05 20:40:56 -0400

    Commit on train

--- hw1/prim.py
+++ hw1/prim.py
@@ -1,7 +1,27 @@
 #!/usr/bin/python
+import pdb
+
+def addVertex(G, node, visited, edges):
+    visited.add(node)
+    for x in G[node]:
+        if x[0] not in visited:
+            edges.append((x[0], x[1]))
+
+def addCheapest(G, visited, edges):
+    minCost = float('inf')
+    minKey = None
+    for x in edges:
+        if x[0] not in visited:
+            minCost = x[1]
+            minKey = x[0]
+    addVertex(G, minKey, visited, edges)
+    return minCost
 
 def mstWeight(G):
-    visited = {}
+    visited = set()
+    edges = []
+    addVertex(G, 1, visited, edges)
+    cost = 0
     while len(visited) < len(G):
-        return 0
-    return 0
+        cost += addCheapest(G, visited, edges)
+    return cost
--- hw1/prim.test.py
+++ hw1/prim.test.py
@@ -3,12 +3,38 @@ import unittest
 from prim import *
 
 def parseFile(fileName):
-    with open(filename, 'r') as f:
-        return
+    G = {}
+    with open(fileName, 'r') as f:
+        f.readline() #kill header
+        for line in f:
+            parts = line.split()
+            v1 = int(parts[0])
+            v2 = int(parts[1])
+            cost = int(parts[2])
+
+            if v1 in G:
+                G[v1].append((v2, cost))
+            else:
+                G[v1] = [(v2, cost)]
+
+            if v2 in G:
+                G[v2].append((v1, cost))
+            else:
+                G[v2] = [(v1, cost)]
+        return G
 
 class PrimTest(unittest.TestCase):
+    def test_count(self):
+        G = parseFile('edges.txt')
+        self.assertEqual(500, len(G.keys()))
+
+    def test_unit(self):
+        G = parseFile('7_prim.txt')
+        self.assertEqual(7, mstWeight(G))
+
     def test_simple(self):
-        self.assertEqual(0, mstWeight([]))
+        G = parseFile('edges.txt')
+        #self.assertEqual(0, mstWeight(G))
 
 if __name__ == '__main__':
     unittest.main()
