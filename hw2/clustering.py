0171a1aa12782f24badc1e7afc9441952b11ea42
Benjamin Hipple <brh2113@columbia.edu>
2015-04-12 21:30:27 -0400

    Passing cases for problem 1

--- hw2/clustering.py
+++ hw2/clustering.py
@@ -1,4 +1,35 @@
 #!/usr/bin/python
 
+class Node():
+    def __init__(self):
+        self.leader = self
+        self.clusterSize = 1
+        self.followers = [self]
+
+def union(l1, l2):
+    if l2.clusterSize > l1.clusterSize: return union(l2, l1)
+
+    l1.clusterSize += l2.clusterSize
+    for node in l2.followers:
+        node.leader = l1
+        l1.followers.append(node)
+
 def max_spacing_k_clustering(k, nodes, edges):
-    return 0
+    numClusters = len(nodes) - 1
+    for i in range(len(edges)):
+        v1 = nodes[edges[i][1]]
+        v2 = nodes[edges[i][2]]
+
+        l1 = v1.leader
+        l2 = v2.leader
+
+        if l1 == l2:
+            continue
+
+        if numClusters == k:
+            break
+
+        union(l1, l2)
+        numClusters -= 1
+
+    return edges[i][0]
--- hw2/clustering.test.py
+++ hw2/clustering.test.py
@@ -12,7 +12,18 @@ def hamming_distance(a, b):
 # Distances are positive but not necessarily distinct
 def loadFile(filename):
     edges = []
-    nodes = [None] # Packing in garbage in index 0, to avoid off-by-one confusion
+    nodes = [Node()] # Packing in garbage in index 0, to avoid off-by-one confusion
+    with open(filename, 'r') as fh:
+        n = int(fh.readline())
+        map(lambda x: nodes.append(Node()), [x for x in range(1,n+1)])
+
+        for parts in map(lambda x: x.split(), fh.readlines()):
+            v1 = int(parts[0])
+            v2 = int(parts[1])
+            cost = int(parts[2])
+            edges.append((cost, v1, v2))
+
+    edges = sorted(edges, cmp=lambda x,y: cmp(x[0], y[0]))
     return nodes, edges
 
 # For question 2, we have a much larger graph, with each row in the format:
@@ -23,41 +34,87 @@ def loadFile(filename):
 def loadFile2(filename):
     return {}
 
+class Component_Tester(unittest.TestCase):
+    def test_node(self):
+        a = Node()
+        self.assertEqual(a, a.leader)
+        self.assertEqual(1, a.clusterSize)
+
+        b = Node()
+        self.assertNotEqual(a.leader, b.leader)
+        a.leader = b.leader
+        self.assertEqual(a.leader, b.leader)
+
+    def test_union(self):
+        a = Node()
+        b = Node()
+        c = Node()
+        union(a,b)
+        self.assertEqual(a.leader, b.leader)
+        self.assertEqual(2, a.leader.clusterSize)
+
+        union(b.leader,c.leader)
+        self.assertEqual(3, b.leader.clusterSize)
+        self.assertEqual(b.leader, c.leader)
+        self.assertEqual(a.leader, c.leader)
+        self.assertEqual(a.leader.clusterSize, c.leader.clusterSize)
+
 
 class HW1_Tester(unittest.TestCase):
     def test_loading_file(self):
         [nodes, edges] = loadFile('line.txt')
         self.assertEqual(9, len(nodes))
+        last = edges[0]
+        for edge in edges[1:]:
+            self.assertGreaterEqual(edge[0], last[0])
+            last = edge
 
         [nodes, edges] = loadFile('grid.txt')
         self.assertEqual(10, len(nodes))
+        last = edges[0]
+        for edge in edges[1:]:
+            self.assertGreaterEqual(edge[0], last[0])
+            last = edge
 
         [nodes, edges] = loadFile('simple.txt')
         self.assertEqual(6, len(nodes))
 
-    def d_test_line(self):
-        [nodes, edges] = loadFile('line.txt')
+    def test_simple(self):
+        [nodes, edges] = loadFile('simple.txt')
+        self.assertEqual(5, max_spacing_k_clustering(2, nodes, edges))
 
-        # Total distance as a function of k and nodes
+        [nodes, edges] = loadFile('simple.txt')
+        self.assertEqual(4, max_spacing_k_clustering(3, nodes, edges))
+
+        [nodes, edges] = loadFile('simple.txt')
+        self.assertEqual(3, max_spacing_k_clustering(4, nodes, edges))
+
+    def test_line(self):
+        [nodes, edges] = loadFile('line.txt')
         self.assertEqual(6, max_spacing_k_clustering(2, nodes, edges))
+
+        [nodes, edges] = loadFile('line.txt')
         self.assertEqual(5, max_spacing_k_clustering(3, nodes, edges))
+
+        [nodes, edges] = loadFile('line.txt')
         self.assertEqual(2, max_spacing_k_clustering(4, nodes, edges))
 
-    def d_test_grid(self):
+    def test_grid(self):
         [nodes, edges] = loadFile('grid.txt')
-
-        # Total distance as a function of k and nodes
         self.assertEqual(4472, max_spacing_k_clustering(2, nodes, edges))
-        self.assertEqual(3606, max_spacing_k_clustering(3, nodes, edges))
-        self.assertEqual(1414, max_spacing_k_clustering(4, nodes, edges))
 
+        [nodes, edges] = loadFile('grid.txt')
+        self.assertEqual(3606, max_spacing_k_clustering(3, nodes, edges))
+
+        [nodes, edges] = loadFile('grid.txt')
+        self.assertEqual(1414, max_spacing_k_clustering(4, nodes, edges))
 
     def hw1(self):
         [nodes, edges] = loadFile('clustering1.txt')
         print max_spacing_k_clustering(4, nodes, edges)
 
 class HW2_Tester(unittest.TestCase):
-    def disabled_test_hamming(self):
+    def test_hamming(self):
         self.assertEqual(3, hamming_distance("Benn", "Inna"))
 
         a = "0 1 1 0 0 1 1 0 0 1 0 1 1 1 1 1 1 0 1 0 1 1 0 1"
