b1cd8dc026dc2c8ff691baeb950dfb5909500adc
Benjamin Hipple <brh2113@columbia.edu>
2015-04-18 19:06:15 -0400

    updated with lecture example test case

--- hw3/knapsack.test.py
+++ hw3/knapsack.test.py
@@ -6,20 +6,25 @@ from knapsack import *
 def loadFromFile(filename):
     with open(filename, 'r') as fh:
         header = fh.readline().split()
-        knapsackSize = int(header[0])
+        size = int(header[0])
         lines = map(lambda x: x.split(), fh.readlines())
     items = map(lambda x: (int(x[0]), int(x[1])), lines)
-    return knapsackSize, items
+    return size, items
 
 class TestLoader(unittest.TestCase):
     def test_load(self):
-        knapsackSize, items = loadFromFile('knapsack1.txt')
-        self.assertEqual(10000, knapsackSize)
+        size, items = loadFromFile('knapsack1.txt')
+        self.assertEqual(10000, size)
         self.assertEqual(100, len(items))
 
 class Problem1(unittest.TestCase):
-    def test_hi(self):
-        return
+    def test_class_example(self):
+        size, items = loadFromFile('class_example.txt')
+        self.assertEqual(8, knapsack(size, items))
+
+    def test_hw1(self):
+        size, items = loadFromFile('knapsack1.txt')
+        print "\nHW1 Answer: %s\n" % knapsack(size, items)
 
 if __name__ == "__main__":
     unittest.main()
