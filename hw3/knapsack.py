a7fb783067c0ebcf17886eb62efab4c89f9e7355
Benjamin Hipple <brh2113@columbia.edu>
2015-04-18 19:26:24 -0400

    Finished hw1 knapsack

--- hw3/knapsack.py
+++ hw3/knapsack.py
@@ -1,4 +1,27 @@
 #!/usr/bin/python
 
+def calcDecision(A, items, i, x):
+    skipIt = 0
+    useIt = 0
+
+    if i > 0:
+        skipIt = A[i-1][x]
+
+    remainder = x - items[i][1]
+    if remainder >= 0:
+        useIt = A[i-1][remainder] + items[i][0]
+
+    return max(skipIt, useIt)
+
+
 def knapsack(size, items):
-    return 0
+    n = len(items)
+    A = []
+    for i in range(n):
+        A.append([0]*(size+1))
+
+    for i in range(n):
+        for x in range(size+1):
+            A[i][x] = calcDecision(A, items, i, x)
+
+    return A[n-1][size]
