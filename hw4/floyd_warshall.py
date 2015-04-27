d369706c9857684cbd8a6c6fb698a21f9e3875da
Benjamin Hipple <brh2113@columbia.edu>
2015-04-26 23:42:43 -0400

    Bellman Ford working with small tests, using dictionaries

--- hw4/floyd_warshall.py
+++ hw4/floyd_warshall.py
@@ -8,19 +8,21 @@ class Node():
         self.ins = []
         self.weight = 0
 
-def bellmanInit(nodes, s):
-    A = []
-    for i in range(len(nodes)):
-        A.append([float('inf')] * len(nodes))
+def bellmanInit(G, s):
+    A = {}
+    for i in range(len(G) + 1):
+        A[i] = {}
+        for key in G:
+            A[i][key] = float('inf')
 
     A[0][s] = 0
     return A
 
-def bellmanBody(nodes, A):
-    for i in range(1, len(nodes)):
-        for v in range(1, len(nodes)):
-            minCost = A[i-1][v]
-            for inc in nodes[v].ins:
+def bellmanBody(G, A):
+    for i in range(1, len(G) + 1):
+        for v in G:
+            minCost = A[i-1][v] or float('inf')
+            for inc in G[v].ins:
                 minCost = min(minCost, A[i-1][inc[0]] + inc[1])
             A[i][v] = minCost
     return A
@@ -28,79 +30,27 @@ def bellmanBody(nodes, A):
 def checkCycle(A):
     return A[len(A)-1] != A[len(A)-2]
 
-def bellmanFord(nodes, s):
-    A = bellmanInit(nodes, s)
-    A = bellmanBody(nodes, A)
+def bellmanFord(G, s):
+    A = bellmanInit(G, s)
+    A = bellmanBody(G, A)
     if checkCycle(A):
         return False
 
     return A[len(A)-1]
 
-def johnsonReweighting(nodes):
-    # Preprocess with new start node 0
-    for i in range(1, len(nodes)):
-        nodes[i].ins.append([0, 0])
-        nodes[0].outs.append([i,0])
+def shortestShortestPath(nodeToDists):
+    minPath = float('inf')
+    for node in nodeToDists:
+        minPath = min(minPath, min(node.values()))
+    return minPath
 
-    # Find vertex weights, or a negative cycle
-    dists = bellmanFord(nodes, 0)
-    if not dists:
-        return False # Negative cycle
-
-    # Reweight the edges
-    for i in range(1, len(nodes)):
-        thisNode = nodes[i]
-        thisNode.weight = dists[i]
-
-        for out in thisNode.outs:
-            out[1] = out[1] + dists[i] - dists[out[0]]
-
-    return True
-
-def dijkstra(G, start):
-    D = {}  # Node -> distance
-    P = {}  # Node -> predecessor in path
-    Q = priorityDictionary()  # Node -> cost to add
-    Q[start] = 0
-
-    for v in Q:
-        D[v] = Q[v]
-        for w in G[v]:
-            vwLength = D[v] + G[v][w]
-            if w not in Q or vwLength < Q[w]:
-                Q[w] = vwLength
-                P[w] = v
-
-def postProcess(D, thisWeight, weights):
-    A = []
-    for key in D:
-        A.append(D[key] - weights[key] + thisWeight)
-    return A
-
-def convertToDictRep(nodes):
-    G = {}
-    weights = {}
-    for i in range(1,len(nodes)):
-        weights[i] = nodes[i].weight
-        outs = {}
-        for out in nodes[i].outs:
-            outs[out[0]] = out[1]
-        G[i] = outs
-
-    return G, weights
-
-def allPairsShortestPaths(nodes):
-    if not johnsonReweighting(nodes):
-        print "\nNegative cycle detected."
-        return False
-
-    G, weights = convertToDictRep(nodes)
-    nodeNumToDistAllOthers = [[float('inf')]] # OBOE filler
-    for s in range(1, len(nodes)):
-        D = dijkstra(G, G[s])
-        distances = postProcess(D, nodes[s].weight, weights)
+def allPairsShortestPaths(G):
+    nodeNumToDistAllOthers = []
+    for s in G:
         print s
-        nodeNumToDistAllOthers.append(distances)
+        thisNodesDists = bellmanFord(G, s)
+        if not thisNodesDists:
+            return False
+        nodeNumToDistAllOthers.append(thisNodesDists)
 
-    print nodeNumToDistAllOthers
-    return min([min(x) for x in nodeNumToDistAllOthers])
+    return shortestShortestPath(nodeNumToDistAllOthers)
--- hw4/floyd_warshall.test.py
+++ hw4/floyd_warshall.test.py
@@ -7,46 +7,26 @@ from floyd_warshall import *
 # Each other line is in the format:
 #   [edge i node a][edge i node b][edge i cost]
 def loadFile(filename):
-    nodes = []
+    G = {}
     with open(filename, 'r') as fh:
         n = int(fh.readline().split()[0])
-        # Packing in garbage in index 0, to avoid off-by-one confusion
-        map(lambda x: nodes.append(Node()), [x for x in range(0,n+1)])
+        for i in range(1,n+1):
+            G[i] = Node()
 
         for parts in map(lambda x: x.split(), fh.readlines()):
             v1 = int(parts[0])
             v2 = int(parts[1])
             cost = int(parts[2])
-            nodes[v1].outs.append([v2, cost])
-            nodes[v2].ins.append([v1, cost])
-    return nodes
+            G[v1].outs.append([v2, cost])
+            G[v2].ins.append([v1, cost])
+    return G
 
 class Component_Tester(unittest.TestCase):
     def test_all_files_get_correct_node_count(self):
-        nodes = loadFile('g1.txt')
-        self.assertEqual(1001, len(nodes))
-        for node in nodes[1:]:
-            self.assertTrue(len(node.outs))
-
-        nodes = loadFile('g2.txt')
-        self.assertEqual(1001, len(nodes))
-
-        nodes = loadFile('g3.txt')
-        self.assertEqual(1001, len(nodes))
-
-    def test_A_initialization(self):
-        nodes = loadFile('g1.txt')
-        A = bellmanInit(nodes, 1)
-        n = len(nodes)
-        self.assertEqual(n, len(A))
-        self.assertEqual(n, len(A[0]))
-
-    def test_johnson_init(self):
-        nodes = loadFile('6.txt')
-        johnsonReweighting(nodes)
-        for node in nodes:
-            for out in node.outs:
-                self.assertGreaterEqual(out[1], 0)
+        G = loadFile('g1.txt')
+        self.assertEqual(1000, len(G))
+        for key in G:
+            self.assertTrue(len(G[key].outs))
 
 class Unit_Tester(unittest.TestCase):
     def test_neg_6(self):
@@ -61,7 +41,8 @@ class Unit_Tester(unittest.TestCase):
         nodes = loadFile('negative_cycle.txt')
         self.assertFalse(allPairsShortestPaths(nodes))
 
-class HW_Runner():
+class HW_Runner(unittest.TestCase):
+    """
     def test_first_graph(self):
         nodes = loadFile('g1.txt')
         print "\nFor file g1, the shortest shortest path is:"
@@ -71,8 +52,9 @@ class HW_Runner():
         nodes = loadFile('g2.txt')
         print "\nFor file g2, the shortest shortest path is:"
         print allPairsShortestPaths(nodes)
+    """
 
-    def test_third_graph(self):
+    def d_test_third_graph(self):
         nodes = loadFile('g3.txt')
         print "\nFor file g3, the shortest shortest path is:"
         print allPairsShortestPaths(nodes)
